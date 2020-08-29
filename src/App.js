cddb46054ba88d8708cd661a9071ed1a07d1ac93
bobo mallick <mallickbobo@gmail.com>
2020-08-29 14:52:57 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from '@material-ui/core';
 import './App.css';
 import News from './components/News/News';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect( () => {
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=a88f826d203d4558831f453c4c3fd824'
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setArticles(data.articles))
+  }, [])
   return (
     <div>
-      <Button color="primary">Hello World</Button>
-      <Button variant="contained" color="secondary">
-          Secondary
-      </Button>
-      <News></News>
+     <h2>Headlines: {articles.length}</h2>
+    {
+      articles.map(article => <News article={article}></News>)
+    }
     </div>
   );
 }
